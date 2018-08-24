6ee3e4ff9895f7b63a1266322b00cb21c7e17ef3
Daniel Trahan <dtrahan41@googlemail.com>
2018-08-24 11:24:50 -0600

    Update matlab.sh

--- matlab.sh
+++ matlab.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
-#SBATCH --nodes=1			# Number of requested nodes
-#SBATCH --time=0:02:00			# Max walltime
-#SBATCH --qos=debug			# Specify debug QOS
-#SBATCH --partition=shas		# Specify Summit haswell nodes
-#SBATCH --output=matlab_%j.out		# Rename standard output file
+#SBATCH --nodes=1                       # Number of requested nodes
+#SBATCH --ntasks=1                      # Number of requested cores per node
+#SBATCH --time=0:02:00                  # Max walltime
+#SBATCH --qos=testing                   # Specify debug QOS
+#SBATCH --partition=shas-testing        # Specify Summit haswell nodes
+#SBATCH --output=matlab_%j.out          # Rename standard output file
 
 
-# Written by:	Shelley Knuth
-# Date:		24 February 2014
-# Updated:      15 July 2016
-# Purpose: 	This script is for the Matlab tutorial.  Its purpose is 
-#		to demonstrate how to submit a serial Matlab job from a 
-#		batch script on the Janus computers.  I also use it to 
-#		demonstrate how to run an external Matlab script	
+# Written by:   Shelley Knuth
+# Modified by:  Daniel Trahan
+# Date:         24 February 2014
+# Updated:      24 August 2018
+# Purpose:      This script is for the Matlab tutorial.  Its purpose is
+#               to demonstrate how to submit a serial Matlab job from a
+#               batch script on the Summit cluster. I also use it to
+#               demonstrate how to run an external Matlab script
 
 # purge all existing modules
 module purge
@@ -22,4 +24,3 @@ module load matlab
 
 # Run matlab without a GUI
 matlab -nodisplay -nodesktop -r "clear; matlab_tic;"
-

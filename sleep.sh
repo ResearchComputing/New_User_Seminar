762e41b958d9a07c5860b468fb23a2885fc185cf
Daniel Trahan <dtrahan41@googlemail.com>
2018-08-24 11:25:44 -0600

    Update sleep.sh

--- sleep.sh
+++ sleep.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-#SBATCH --nodes=1				# Number of requested nodes
-#SBATCH --time=0:01:00				# Max walltime
-#SBATCH --qos=normal				# Specify normal QOS
-#SBATCH --partition=shas			# Specify Summit GPU nodes
-#SBATCH --output=sleep_%j.out			# Rename standard output file
-#SBATCH --job-name=sleep			# Job submission name
-#SBATCH --mail-type=end				# Email you when the job ends 
-###SBATCH --mail-user=<user>@colorado.edu	# Email address to send to	
+#SBATCH --nodes=1                               # Number of requested nodes
+#SBATCH --ntasks=1                              # Number of requested cores per node
+#SBATCH --time=0:01:00                          # Max walltime
+#SBATCH --qos=testing                           # Specify normal QOS
+#SBATCH --partition=shas-testing                # Specify Summit SHAS-testing nodes
+#SBATCH --output=sleep_%j.out                   # Rename standard output file
+#SBATCH --job-name=sleep                        # Job submission name
 
 # Written by:   Shelley Knuth
+# Modified by:  Daniel Trahan
 # Date:         15 July 2016
-# Updated:      3 May 2017
+# Updated:      24 August 2018
 # Purpose:      To demonstrate how to run a batch job on RC resources
 
 

efce2c43972c2606dc09029879c9d713019c9a3f
monaghaa <33913248+monaghaa@users.noreply.github.com>
2024-07-02 10:15:05 -0600

    Update R_code.sh

--- examples/R_code.sh
+++ examples/R_code.sh
@@ -3,14 +3,14 @@
 #SBATCH --ntasks=1                              # Number of requested cores
 #SBATCH --time=0:01:00                          # Max walltime
 #SBATCH --qos=testing                           # Specify debug QOS
-#SBATCH --partition=shas-testing                # Specify Summit haswell nodes
+#SBATCH --partition=amilan                # Specify Summit haswell nodes
 #SBATCH --output=R_code_%j.out                  # Output file name
 
 # purge all existing modules
 module purge
 
 # Load the R module
-module load R/3.3.0
+module load R/4.4.0
 
 # Run R Script
 Rscript R_program.R
